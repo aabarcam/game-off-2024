ea435d11777d1bc38c431338f68ff0843cb72edb
aabarcam <aeduardo10x@gmail.com>
2024-12-01 12:10:44 -0300

    feat(ui): add options menu

--- scripts/menus/options_menu.gd
+++ scripts/menus/options_menu.gd
@@ -2,14 +2,12 @@ extends Control
 
 @onready var music_slider: HSlider = $MusicSlider
 #@onready var sfx_slider: HSlider = $SFXSlider
+@onready var back_button: TextureButton = $BackButton
 
 func _ready() -> void:
 	music_slider.min_value = 0.0001
 	music_slider.step = 0.0001
 	music_slider.value_changed.connect(_on_music_value_changed)
-	
-	if OS.is_debug_build() and get_parent() == get_tree().root:
-		MusicController.play_music("living")
 
 func _on_music_value_changed(value: float) -> void:
 	MusicController.music = value
